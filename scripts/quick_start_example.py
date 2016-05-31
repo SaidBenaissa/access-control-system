1766ff2247bbf4e1d1626580fe818c031085a8ce
D0d0 <jozef.d13@gmail.com>
2016-05-31 21:43:11 +0200

    Add script to manipulate with fibaro plugs and touchago reader

---
+++ scripts/quick_start_example.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Quick start example that presents how to use libnfc"""
+
+from __future__ import print_function
+import nfc
+
+print('Version: ', nfc.__version__)
+
+context = nfc.init()
+pnd = nfc.open(context)
+if pnd is None:
+    print('ERROR: Unable to open NFC device.')
+    exit()
+
+if nfc.initiator_init(pnd) < 0:
+    nfc.perror(pnd, "nfc_initiator_init")
+    print('ERROR: Unable to init NFC device.')
+    exit()
+
+print('NFC reader: %s opened' % nfc.device_get_name(pnd))
+
+i = 0
+while (True):
+    print('----------------------------------------------------------------', i)
+    nmMifare = nfc.modulation()
+    nmMifare.nmt = nfc.NMT_ISO14443A
+    nmMifare.nbr = nfc.NBR_106
+
+    nt = nfc.target()
+    ret = nfc.initiator_select_passive_target(pnd, nmMifare, 0, 0, nt)
+    print('The following (NFC) ISO14443A tag was found:')
+    print('    ATQA (SENS_RES): ', end='')
+    nfc.print_hex(nt.nti.nai.abtAtqa, 2)
+    id = 1
+    if nt.nti.nai.abtUid[0] == 8:
+        id = 3
+    print('       UID (NFCID%d): ' % id, end='')
+    nfc.print_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen)
+    print('      SAK (SEL_RES): ', end='')
+    print(nt.nti.nai.btSak)
+    if nt.nti.nai.szAtsLen:
+        print('          ATS (ATR): ', end='')
+        nfc.print_hex(nt.nti.nai.abtAts, nt.nti.nai.szAtsLen)
+    i = i + 1
+
+nfc.close(pnd)
+nfc.exit(context)
