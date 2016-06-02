59fbe462d84d3ce737638cf0cb30a2b0cc908da2
D0d0 <jozef.d13@gmail.com>
2016-06-02 12:49:43 +0200

    Add basic thread which runs NFC reader

--- accesscontrolsystem/wsgi.py
+++ accesscontrolsystem/wsgi.py
@@ -11,6 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from scripts.quick_start_example import myThread
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "accesscontrolsystem.settings")
+
+thread1 = myThread()
+thread1.start()
 
 application = get_wsgi_application()
--- scripts/quick_start_example.py
+++ scripts/quick_start_example.py
@@ -1,49 +1,14 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import threading
+import time
 
-"""Quick start example that presents how to use libnfc"""
 
-from __future__ import print_function
-import nfc
+class myThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.counter = 0
 
-print('Version: ', nfc.__version__)
-
-context = nfc.init()
-pnd = nfc.open(context)
-if pnd is None:
-    print('ERROR: Unable to open NFC device.')
-    exit()
-
-if nfc.initiator_init(pnd) < 0:
-    nfc.perror(pnd, "nfc_initiator_init")
-    print('ERROR: Unable to init NFC device.')
-    exit()
-
-print('NFC reader: %s opened' % nfc.device_get_name(pnd))
-
-i = 0
-while (True):
-    print('----------------------------------------------------------------', i)
-    nmMifare = nfc.modulation()
-    nmMifare.nmt = nfc.NMT_ISO14443A
-    nmMifare.nbr = nfc.NBR_106
-
-    nt = nfc.target()
-    ret = nfc.initiator_select_passive_target(pnd, nmMifare, 0, 0, nt)
-    print('The following (NFC) ISO14443A tag was found:')
-    print('    ATQA (SENS_RES): ', end='')
-    nfc.print_hex(nt.nti.nai.abtAtqa, 2)
-    id = 1
-    if nt.nti.nai.abtUid[0] == 8:
-        id = 3
-    print('       UID (NFCID%d): ' % id, end='')
-    nfc.print_hex(nt.nti.nai.abtUid, nt.nti.nai.szUidLen)
-    print('      SAK (SEL_RES): ', end='')
-    print(nt.nti.nai.btSak)
-    if nt.nti.nai.szAtsLen:
-        print('          ATS (ATR): ', end='')
-        nfc.print_hex(nt.nti.nai.abtAts, nt.nti.nai.szAtsLen)
-    i = i + 1
-
-nfc.close(pnd)
-nfc.exit(context)
+    def run(self):
+        while (True):
+            time.sleep(1)
+            self.counter += 1
+            print(self.counter)
