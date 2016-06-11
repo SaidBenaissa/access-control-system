139374c80861525198e3f0a14b7a3867b464211f
D0d0 <jozef.d13@gmail.com>
2016-06-11 23:47:12 +0200

    Handle connect in TCP

--- scripts/TCPClient.js
+++ scripts/TCPClient.js
@@ -11,15 +11,16 @@ TCPClient.prototype = {
     start: function () {
         if (config.ENABLE.FIBARO) {
             this._client = new net.Socket();
-            this._client.connect(9876, config.SCRIPT_PATH.FIBARO, function () {
-                console.log('TCP client started');
-                this._client.write('Hello, server! Love, Client.');
-            });
+            this._client.connect(9876, config.SCRIPT_PATH.FIBARO, this.handleConnect.bind(this));
             this._client.on('data', this.handleData.bind(this));
             this._client.on('close', this.handleClose.bind(this));
         } else {
             console.log("TCP client to Fibaro is turned off in config file.");
         }
+    },
+    handleConnect: function () {
+        console.log('TCP client started');
+        this._client.write('Hello, server! Love, Client.');
     },
     handleData: function (data) {
         console.log(data);
