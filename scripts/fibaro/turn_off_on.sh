6e1f41f23a56c7f7287658a9922616a9b2ad9848
D0d0 <jozef.d13@gmail.com>
2016-06-18 20:14:41 +0200

    Send all params to switch script

--- scripts/fibaro/turn_off_on.sh
+++ scripts/fibaro/turn_off_on.sh
@@ -5,4 +5,4 @@ export LD_LIBRARY_PATH=$ZWAY_LIB_DIR:$LD_LIBRARY_PATH;
 export DYLD_LIBRARY_PATH=$ZWAY_LIB_DIR:$DYLD_LIBRARY_PATH;
 #./compile.sh;
 echo "Turn off on";
-/home/pi/Desktop/access-control-system/scripts/fibaro/turn_off_on $1 $2;
+/home/pi/Desktop/access-control-system/scripts/fibaro/turn_off_on "$@";
